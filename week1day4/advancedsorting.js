c2d1d153591d50fb2a312d16d12785c388d165b4
Ayesha <ayesha_04@yahoo.com>
2017-06-29 21:05:12 +0000

    CallBack Maps my version

--- week1day4/advancedsorting.js
+++ week1day4/advancedsorting.js
@@ -2,7 +2,8 @@ var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
   { id: 3, name: "alex",     age: 22 },
-  { id: 4, name: "alex",     age: 30 }
+  { id: 4, name: "alex",     age: 30 },
+
 ];
 
  var result = students.sort(function(a,b) {
